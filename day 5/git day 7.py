db47c3b001ca8859b80da8624a77839313903687
sambiddha <sambidh9@gmail.com>
2023-05-03 08:33:55 +0545

    first commit

--- day 5/git day 7.py
+++ day 5/git day 7.py
@@ -1,1 +1,10 @@
 # git init
+# git add .  or (git add --all)
+# git commit -m "commit_message>"
+
+# git status #Shows the current status of all git staged and unstaged files
+
+
+# Setting git configuration
+# 1. git config --global user.name "<your-username>"
+# 2. git config --global user.email "<your-email>"
